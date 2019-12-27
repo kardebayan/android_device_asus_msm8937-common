1597f4cbf21746e7f760c041f53e3ee5b5e4039c
ZeeLog <Danjel.Zammit@gmail.com>
2019-12-27 22:44:41 +0100

    msm8937-common: Convert lights HAL into a native binderized HAL
    Change-Id: Id0ea4dbf5ac922abca059738f41f27b1f54c32a9

--- msm8937.mk
+++ msm8937.mk
@@ -215,11 +215,7 @@ PRODUCT_COPY_FILES += \
 
 # Lights
 PRODUCT_PACKAGES += \
-    lights.msm8937
-
-PRODUCT_PACKAGES += \
-    android.hardware.light@2.0-impl \
-    android.hardware.light@2.0-service
+    android.hardware.light@2.0-service.asus_8937
 
 # LiveDisplay
 PRODUCT_PACKAGES += \
