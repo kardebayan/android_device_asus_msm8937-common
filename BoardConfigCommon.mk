a1b75cf42154ec17d7ffefbccd041748f74975e0
LuK1337 <priv.luk@gmail.com>
2020-09-18 10:57:02 +0200

    msm8937-common: Update qcom sepolicy include
    Change-Id: I7167575e82eb8c5de2d822643261757edda73a07

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -191,7 +191,7 @@ TARGET_RECOVERY_UPDATER_LIBS := librecovery_updater_asus
 TARGET_RELEASETOOLS_EXTENSIONS := $(VENDOR_PATH)
 
 # SELinux
-include device/qcom/sepolicy-legacy-um/sepolicy.mk
+include device/qcom/sepolicy-legacy-um/SEPolicy.mk
 BOARD_SEPOLICY_DIRS += $(VENDOR_PATH)/sepolicy
 
 # Treble
